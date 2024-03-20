2c107c2730db2b35bb53f9fe1b2d2f6631118d37
Rayhanul Islam <r97yhan@gmail.com>
2024-03-20 13:27:33 -0500

    uploading file to pineCone using commandline

--- query.py
+++ query.py
@@ -10,26 +10,26 @@ from rag_manager import *;
 
 if __name__=="__main__":
 
-    file_name="Project-Boosting.pdf"
-    query="what is boosting?"
-    key_manager= Key_parser()
-    my_keys=key_manager.parsing_keys()
+    file_name=""
+    # query="what is Renyi differnetial privacy?"
+    # key_manager= Key_parser()
+    # my_keys=key_manager.parsing_keys()
 
-    pdf_manager= Data_Loader(file_name)
-    text_tokens=pdf_manager.get_text_from_pdf()
+    # pdf_manager= Data_Loader(file_name)
+    # text_tokens=pdf_manager.get_text_from_pdf()
 
-    text_embedder=Text_embedder(text=text_tokens, keys=my_keys)
+    # text_embedder=Text_embedder(text=text_tokens, keys=my_keys)
 
-    index, index_name, embed =text_embedder.get_indexing(text_tokens)
+    # index, index_name, embed =text_embedder.get_indexing(text_tokens)
 
 
-    rag_manager=Rag_Manager(my_keys, index, embed )
+    # rag_manager=Rag_Manager(my_keys, index, embed )
 
-    ans= rag_manager.answer_query(query)
+    # ans= rag_manager.answer_query(query)
 
-    text_embedder.remove_index(index_name)
+    # # text_embedder.remove_index(index_name)
 
-    print(ans) 
+    # print(ans) 
 
 
 

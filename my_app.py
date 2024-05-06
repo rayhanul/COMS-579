bb91c1cbd3ba44fde57aae0efe0113ca055dcbcd
Rayhanul Islam <r97yhan@gmail.com>
2024-05-06 01:18:00 -0500

    webapp- asking question

---
+++ my_app.py
@@ -0,0 +1,40 @@
+import funix
+
+from pdf_manager import *;
+from embedder import *; 
+from parsing_keys import *;
+from rag_manager import *;
+
+@funix.funix(
+  argument_labels={
+    "x": "The number to be squared"
+  }
+)
+
+def Ask_Question(query: str) -> str:
+    key_manager= Key_parser()
+    my_keys=key_manager.parsing_keys()
+
+
+
+
+    rag_manager=Rag_Manager(my_keys )
+
+    ans= rag_manager.answer_query(query)
+
+
+
+    
+    return ans["answer"]
+
+
+@funix.funix(
+  argument_labels={
+    "x": "The number to be squared"
+  }
+)
+
+def Index_New_File(x: int) -> int:
+    return x * x
+
+
